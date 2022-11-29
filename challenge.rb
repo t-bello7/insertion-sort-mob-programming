9872153b993dd7a09fa62941f1631203c155204e
t-bello7 <bello4aus@gmail.com>
2022-11-29 08:37:00 +0100

    sort the array using insertion sort

--- challenge.rb
+++ challenge.rb
@@ -1,9 +1,21 @@
 def insertion_sort(array)
   # write your code here
-  
+  for a in 1..array.length do 
+    currPosition = a
+    valueToInsert = array[a]
+    while  valueToInsert != nil && (array[currPosition-1] > valueToInsert)
+      array[currPosition] = array[currPosition - 1]
+      currPosition = currPosition - 1
+    end
+    if valueToInsert != nil 
+      array[currPosition] = valueToInsert
+      puts "#{array} \n"
+    end
+  end
+  array
 end
 
-insertion_sort([1, 3, 2])
+insertion_sort([1, 3, 2]) 
 # => 1 3 3
 #    1 2 3
 
